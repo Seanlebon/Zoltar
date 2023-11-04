233b9a281bf890a43d39ac333dab72e0328e3a22
SeanLeBon <sean.chen0818@gmail.com>
2023-11-04 02:59:28 -0700

    base bot template created

---
+++ core/bot.py
@@ -0,0 +1,45 @@
+import json
+import logging
+import os
+import platform
+import random
+import sys
+
+import discord
+from discord.ext import commands, tasks
+from discord.ext.commands import Context
+from dotenv import load_dotenv
+
+intents = discord.Intents.default()
+
+class ZoltarBot(commands.Bot):
+    def __init__(self, intents) -> None:
+        super().__init__(
+            command_prefix=commands.when_mentioned_or('!'),
+            intents=intents,
+            help_command=None,
+        )
+        """
+        This creates custom bot variables so that we can access these variables in cogs more easily.
+
+        For example, The config is available using the following code:
+        - self.config # In this class
+        - bot.config # In this file
+        - self.bot.config # In cogs
+        """
+
+    async def load_cogs(self) -> None:
+        """
+        The code in this function is executed whenever the bot will start.
+        """
+        for file in os.listdir(f"{os.path.realpath(os.path.dirname(__file__))}/../cogs"):
+            if file.endswith(".py"):
+                extension = file[:-3]
+                try:
+                    await self.load_extension(f"cogs.{extension}")
+                    self.logger.info(f"Loaded extension '{extension}'")
+                except Exception as e:
+                    exception = f"{type(e).__name__}: {e}"
+                    self.logger.error(
+                        f"Failed to load extension {extension}\n{exception}"
+                    )
---
+++ main.py
@@ -0,0 +1,16 @@
+import discord
+import os
+from dotenv import load_dotenv
+from core.bot import ZoltarBot
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+def main():
+    intents = discord.Intents.default()
+    bot = ZoltarBot(intents=intents)
+    bot.run(TOKEN)
+
+if __name__ == "__main__":
+    main()
